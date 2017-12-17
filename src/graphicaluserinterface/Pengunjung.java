b466b5fc8e21965b89f89f611e5e505c2996fc9e
Unknown <helius_aim00@yahoo.com>
2017-12-17 12:05:38 +0700

    bababba

---
+++ src/graphicaluserinterface/Pengunjung.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphicaluserinterface;
+
+/**
+ *
+ * @author faldi
+ */
+public class Pengunjung {
+    private String noKTP;
+    private String nama;
+    private String jenKel;
+    private String handuk;
+    private String cabang;
+    private String namaPaketFitness;
+    private String hargaPaketFitness;
+
+    public String getNoKTP() {
+        return noKTP;
+    }
+
+    public void setNoKTP(String noKTP) {
+        this.noKTP = noKTP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJenKel() {
+        return jenKel;
+    }
+
+    public void setJenKel(String jenKel) {
+        this.jenKel = jenKel;
+    }
+
+    public String getHanduk() {
+        return handuk;
+    }
+
+    public void setHanduk(String handuk) {
+        this.handuk = handuk;
+    }
+
+    public String getCabang() {
+        return cabang;
+    }
+
+    public void setCabang(String cabang) {
+        this.cabang = cabang;
+    }
+
+    public String getNamaPaketFitness() {
+        return namaPaketFitness;
+    }
+
+    public void setNamaPaketFitness(String namaPaketFitness) {
+        this.namaPaketFitness = namaPaketFitness;
+    }
+
+    public String getHargaPaketFitness() {
+        return hargaPaketFitness;
+    }
+
+    public void setHargaPaketFitness(String hargaPaketFitness) {
+        this.hargaPaketFitness = hargaPaketFitness;
+    }
+
+    public void cekHarga() {
+
+    }}
